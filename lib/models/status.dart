25fe8533e5d3e2ae68654bc7163d57fb53af99b3
matekaszas13 <matekaszas17@gmail.com>
2023-08-07 17:05:36 +0200

    feat: add json serializable to status enum

--- lib/models/status.dart
+++ lib/models/status.dart
@@ -1,1 +1,10 @@
-enum Status { active, locked }
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@JsonEnum(valueField: 'status')
+enum Status {
+  active('active'),
+  locked('locked');
+
+  const Status(this.status);
+  final String status;
+}
