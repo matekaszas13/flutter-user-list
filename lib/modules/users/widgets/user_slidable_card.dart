98c1fa84fa86ee7d59a527d9e717872c623417e5
matekaszas13 <matekaszas17@gmail.com>
2023-08-08 09:37:11 +0200

    feat: add container around slidable card element

--- lib/modules/users/widgets/user_slidable_card.dart
+++ lib/modules/users/widgets/user_slidable_card.dart
@@ -25,40 +25,43 @@ class UserSlidableCard extends ConsumerWidget {
     final formattedDate =
         DateFormat('yyyy/MM/dd HH:mm').format(user.createdAt.toLocal());
 
-    return Slidable(
-      key: ValueKey(user.id),
-      endActionPane: ActionPane(
-          motion: const DrawerMotion(),
-          dragDismissible: false,
-          children: [
-            SlidableAction(
-              onPressed: (context) => openUpdateUserBottomSheet(
-                  context, user.firstName, user.lastName, user.id, true),
-              backgroundColor: const Color.fromARGB(100, 92, 70, 156),
-              foregroundColor: Colors.white,
-              icon: Icons.edit,
-              label: 'Edit',
-              flex: 1,
-            ),
-            SlidableAction(
-              onPressed: (context) => openDeleteConfirmationalDialog(
-                  context, user.id, user.fullName),
-              backgroundColor: const Color.fromARGB(231, 214, 54, 54),
-              foregroundColor: Colors.white,
-              icon: Icons.delete,
-              label: 'Delete',
-              flex: 1,
-            ),
-          ]),
-      child: Card(
-        child: ListTile(
-          title: Text(user.fullName),
-          subtitle: Text(formattedDate),
-          trailing: IconButton(
-              onPressed: () => handleStatusChange(user),
-              icon: user.status == Status.active
-                  ? const Icon(Icons.lock_open)
-                  : const Icon(Icons.lock)),
+    return Container(
+      margin: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+      child: Slidable(
+        key: ValueKey(user.id),
+        endActionPane: ActionPane(
+            motion: const DrawerMotion(),
+            dragDismissible: false,
+            children: [
+              SlidableAction(
+                onPressed: (context) => openUpdateUserBottomSheet(
+                    context, user.firstName, user.lastName, user.id, true),
+                backgroundColor: const Color.fromARGB(100, 92, 70, 156),
+                foregroundColor: Colors.white,
+                icon: Icons.edit,
+                label: 'Edit',
+                flex: 1,
+              ),
+              SlidableAction(
+                onPressed: (context) => openDeleteConfirmationalDialog(
+                    context, user.id, user.fullName),
+                backgroundColor: const Color.fromARGB(255, 214, 54, 54),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+                flex: 1,
+              ),
+            ]),
+        child: Card(
+          child: ListTile(
+            title: Text(user.fullName),
+            subtitle: Text(formattedDate),
+            trailing: IconButton(
+                onPressed: () => handleStatusChange(user),
+                icon: user.status == Status.active
+                    ? const Icon(Icons.lock_open)
+                    : const Icon(Icons.lock)),
+          ),
         ),
       ),
     );
