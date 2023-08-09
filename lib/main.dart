b5790ce18171811a0aeb64857ee2db412ff6ebda
matekaszas13 <matekaszas17@gmail.com>
2023-08-09 09:32:11 +0200

    feat: add translation to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_user_list/i18n/i18n_provider.dart';
 import 'package:flutter_user_list/modules/users/screens/users_screen.dart';
 import 'package:flutter_user_list/utils/theme_data_provider.dart';
 import 'package:flutter_user_list/utils/theme_mode_value_provider.dart';
@@ -20,9 +23,28 @@ class App extends HookConsumerWidget {
 
     final isDarkMode = ref.watch(themeModeValueProvider);
 
+    final locale = ref.watch(i18nProvider.select((locale) => locale));
+
     return MaterialApp(
-        title: 'Users List',
-        theme: ref.watch(customThemeProvider).getAppTheme(context, isDarkMode),
-        home: const UsersScreen());
+      title: 'Users List',
+      theme: ref.watch(customThemeProvider).getAppTheme(context, isDarkMode),
+      locale: locale,
+      localizationsDelegates: [
+        FlutterI18nDelegate(
+          translationLoader: FileTranslationLoader(
+            basePath: 'lib/i18n/locales',
+          ),
+        ),
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('hu'),
+      ],
+
+      home: const UsersScreen(),
+      // home: const UsersScreen(),
+    );
   }
 }
