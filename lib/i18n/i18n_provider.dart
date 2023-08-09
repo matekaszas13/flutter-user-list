9c6c20a84e65f6c3e8230a522424cb6a7e78efa7
matekaszas13 <matekaszas17@gmail.com>
2023-08-09 09:03:07 +0200

    feat: add i18n provider

---
+++ lib/i18n/i18n_provider.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_user_list/i18n/i18n.dart';
+
+final i18nProvider = AutoDisposeNotifierProvider<I18nProvider, Locale?>(I18nProvider.new);
+
+class I18nProvider extends AutoDisposeNotifier<Locale?> {
+  @override
+  Locale? build({Locale? locale}) => locale;
+
+  BuildContext? context;
+
+  Locale get platformLocale => Locale(Platform.localeName.substring(0, 2));
+
+  void initializeAppContext(BuildContext context) {
+    this.context = context;
+  }
+
+  Future setLocale(Locale? locale) async {
+    assert(context != null, "context is not set in I18nProvider");
+    await FlutterI18n.refresh(context!, locale);
+    state = locale;
+  }
+
+  Locale getCurrentLocale() {
+    assert(context != null, "context is not set in I18nProvider");
+    return FlutterI18n.currentLocale(context!) ?? const Locale('de');
+  }
+
+  String tr(String key) {
+    return context?.tr(key) ?? "context is not set in I18nProvider, so cannot retrieve translation for $key";
+  }
+}
