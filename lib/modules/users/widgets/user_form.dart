e8ed14f45ee57cf4fd9977072274a47c7c6d1396
matekaszas13 <matekaszas17@gmail.com>
2023-08-14 14:56:50 +0200

    feat: add user form for add/update user

---
+++ lib/modules/users/widgets/user_form.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_user_list/presentation/utils/form_handler.dart';
+import 'package:flutter_user_list/presentation/widgets/input_field/field_form.dart';
+import 'package:flutter_user_list/presentation/widgets/input_field/input_field.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class UserForm extends HookConsumerWidget {
+  const UserForm({
+    required this.formHandler,
+    super.key,
+  });
+  final FormHandler<FormHandlerState> formHandler;
+
+  @override
+  Widget build(context, ref) {
+    return FieldForm(
+      isScrollControlled: true,
+      fields: formHandler.formHolder.fields,
+      formHandler: formHandler,
+      fieldBuilder: (context, field) {
+        return InputField<dynamic>(
+          field: field,
+          formHandler: formHandler,
+        );
+      },
+    );
+  }
+}
