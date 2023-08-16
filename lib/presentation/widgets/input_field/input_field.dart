ae57d9e87056c0fcd01378e5474eead7dcbbebde
matekaszas13 <matekaszas17@gmail.com>
2023-08-16 17:32:48 +0200

    fix: fix selected option highlight logic

--- lib/presentation/widgets/input_field/input_field.dart
+++ lib/presentation/widgets/input_field/input_field.dart
@@ -151,7 +151,7 @@ class InputField<T> extends HookConsumerWidget {
       return showSelectFieldSheet(
         context,
         title: title,
-        selectedOption: field.initialValue,
+        selectedOption: field.draftValue ?? field.initialValue,
         options: options,
         onSelect: (value) {
           formHandlerOrDefault?.setFieldValue(fieldKey, value);
