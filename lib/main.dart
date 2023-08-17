9e10a8628b5e97a9d084e2f4c8eb901ba02d6ff1
matekaszas13 <matekaszas17@gmail.com>
2023-08-17 16:36:08 +0200

    feat: use platform brighness for theme mode on first render

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_user_list/i18n/i18n.dart';
 import 'package:flutter_user_list/modules/users/screens/users_screen.dart';
 import 'package:flutter_user_list/presentation/theme/scale.dart';
@@ -11,7 +12,7 @@ void main() {
   runApp(const ProviderScope(child: App()));
 }
 
-class App extends ConsumerWidget {
+class App extends HookConsumerWidget {
   const App({super.key});
 
   @override
@@ -24,6 +25,16 @@ class App extends ConsumerWidget {
       designSize: const Size(360, 640),
     );
     final isDarkMode = ref.watch(themeModeValueProvider);
+
+    useEffect(() {
+      Future(
+        () {
+          ref
+              .read(themeModeValueProvider.notifier)
+              .setThemeMode(themeMode: MediaQuery.of(context).platformBrightness == Brightness.dark);
+        },
+      );
+    }, []);
 
     return CustomTheme(
       isDarkMode: isDarkMode,
